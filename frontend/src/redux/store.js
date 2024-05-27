33ffdb0aedddb1a53d570f8682e7f4d2d2ea81d2
SHIVANSH VASU <86513644+ShivaMani02@users.noreply.github.com>
2024-05-27 16:39:15 +0530

    Update store.js

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -20,4 +20,4 @@ const store = configureStore({
 
 export default store;
 
-export const server = 'http://localhost:4000/api/v1';
+export const server = 'https://course-bundler-alpha.onrender.com/api/v1';
